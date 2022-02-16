240ffc6653162d9234bb69943a64d9c4c5b21db6
Ooi Si Sheng <sisheng1998@gmail.com>
2022-02-16 22:23:23 +0800

    fix role verification

--- server/middleware/verifyRoles.js
+++ server/middleware/verifyRoles.js
@@ -2,9 +2,9 @@ const verifyRoles = (minRoles) => {
 	return (req, res, next) => {
 		if (!req?.user) return res.sendStatus(401)
 
-		const result = req.user.roles
-			.map((lab) => lab.role >= minRoles)
-			.find((val) => val === true)
+		const result = req.user.roles.find(
+			(lab) => lab.lab._id.toString() === req.body.labId && lab.role >= minRoles
+		)
 
 		if (!result) return res.sendStatus(401)
 		next()
