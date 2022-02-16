31303b1e27388caf250d7fe4a3fdd24f286f1641
Ooi Si Sheng <sisheng1998@gmail.com>
2022-02-16 11:49:01 +0800

    improve UI

--- client/src/components/layouts/components/Header.jsx
+++ client/src/components/layouts/components/Header.jsx
@@ -8,7 +8,7 @@ import UserOption from './UserOption'
 const Header = () => {
 	return (
 		<header className='fixed top-0 left-0 right-0 z-10 flex items-center justify-between border-b border-gray-300 bg-white p-4 shadow-sm'>
-			<div className='flex items-center'>
+			<div className='mr-6 flex items-center'>
 				<Link className='mr-4' to='/'>
 					<img
 						className='h-8'
@@ -21,7 +21,7 @@ const Header = () => {
 				<LabSelection />
 			</div>
 
-			<div className='flex items-center'>
+			<div className='flex flex-1 items-center justify-end'>
 				<Search />
 				<div className='mx-5 h-6 border-l border-gray-300'></div>
 				<Notification />
--- client/src/components/layouts/components/Search.jsx
+++ client/src/components/layouts/components/Search.jsx
@@ -6,14 +6,14 @@ const Search = () => {
 		<form
 			autoComplete='off'
 			spellCheck='false'
-			className='relative flex items-center rounded-lg hover:shadow-sm'
+			className='relative flex w-full max-w-sm flex-1 items-center rounded-lg hover:shadow-sm'
 		>
 			<input
 				type='text'
 				name='search'
 				id='search'
 				placeholder='Search inventory'
-				className='w-96 pr-8 pl-7 text-sm shadow-none'
+				className='w-full pr-8 pl-7 text-sm shadow-none'
 			/>
 			<SearchIcon className='absolute left-2 h-4 w-4 cursor-pointer text-gray-400 transition hover:text-indigo-600' />
 			<AdjustmentsIcon className='absolute right-2 h-5 w-5 cursor-pointer text-gray-400 transition hover:text-indigo-600' />
