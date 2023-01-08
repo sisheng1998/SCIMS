bf39a45ee0132b61129331f9150aa1c76efd8b77
Ooi Si Sheng <sisheng1998@gmail.com>
2023-01-08 22:45:51 +0800

    update backups table styling

--- client/src/components/app/admin/backup-restore/components/BackupsTable.jsx
+++ client/src/components/app/admin/backup-restore/components/BackupsTable.jsx
@@ -120,90 +120,86 @@ const BackupsTable = ({ backups }) => {
         </div>
       </Filters>
 
-      <div className='mb-5 overflow-hidden rounded-lg border border-gray-200 bg-gray-50 pb-3 shadow'>
-        <div className='overflow-x-auto'>
-          <div className='border-b border-gray-200'>
-            <table className='min-w-full divide-y divide-gray-200 whitespace-nowrap'>
-              <thead className='bg-gray-50'>
+      <div className='mb-5 rounded-lg border border-gray-200 bg-gray-50 pb-3 shadow'>
+        <div className='border-b border-gray-200'>
+          <table className='min-w-full divide-y divide-gray-200 whitespace-nowrap'>
+            <thead className='bg-gray-50'>
+              <tr>
+                {tableHeaders.map((header) => (
+                  <th
+                    scope='col'
+                    key={header.key}
+                    className='px-6 py-3 text-left font-medium text-gray-500'
+                  >
+                    {header.sortable ? (
+                      <SortButton
+                        columnKey={header.key}
+                        onClick={() => changeSortOrder(header.key)}
+                        {...{ sortOrder, sortKey }}
+                      >
+                        {header.label}
+                      </SortButton>
+                    ) : (
+                      header.label
+                    )}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+
+            <tbody className='divide-y divide-gray-200 bg-white'>
+              {currentItems.length === 0 ? (
                 <tr>
-                  {tableHeaders.map((header) => (
-                    <th
-                      scope='col'
-                      key={header.key}
-                      className='px-6 py-3 text-left font-medium text-gray-500'
-                    >
-                      {header.sortable ? (
-                        <SortButton
-                          columnKey={header.key}
-                          onClick={() => changeSortOrder(header.key)}
-                          {...{ sortOrder, sortKey }}
-                        >
-                          {header.label}
-                        </SortButton>
-                      ) : (
-                        header.label
-                      )}
-                    </th>
-                  ))}
+                  <td
+                    className='px-6 py-4 text-center'
+                    colSpan={tableHeaders.length}
+                  >
+                    No record found.
+                  </td>
                 </tr>
-              </thead>
+              ) : (
+                currentItems.map((backup, index) => {
+                  let classes
 
-              <tbody className='divide-y divide-gray-200 bg-white'>
-                {currentItems.length === 0 ? (
-                  <tr>
-                    <td
-                      className='px-6 py-4 text-center'
-                      colSpan={tableHeaders.length}
-                    >
-                      No record found.
-                    </td>
-                  </tr>
-                ) : (
-                  currentItems.map((backup, index) => {
-                    let classes
+                  if (backup.type === BACKUP_TYPE.auto) {
+                    classes = 'bg-blue-100 text-blue-600'
+                  } else {
+                    // Manual
+                    classes = 'bg-yellow-100 text-yellow-600'
+                  }
 
-                    if (backup.type === BACKUP_TYPE.auto) {
-                      classes = 'bg-blue-100 text-blue-600'
-                    } else {
-                      // Manual
-                      classes = 'bg-yellow-100 text-yellow-600'
-                    }
+                  return (
+                    <tr key={index} className='hover:bg-indigo-50/30'>
+                      <td className='px-6 py-4'>{backup.name}</td>
 
-                    return (
-                      <tr key={index} className='hover:bg-indigo-50/30'>
-                        <td className='px-6 py-4'>{backup.name}</td>
+                      <td className='px-6 py-4'>
+                        <span
+                          className='tooltip'
+                          data-tooltip={DateTime(backup.date)}
+                        >
+                          {FromNow(backup.date)}
+                        </span>
+                      </td>
 
-                        <td className='px-6 py-4'>
-                          <span
-                            className='tooltip'
-                            data-tooltip={DateTime(backup.date)}
-                          >
-                            {FromNow(backup.date)}
-                          </span>
-                        </td>
+                      <td className='px-6 py-4'>
+                        <span
+                          className={`inline-flex rounded-full px-3 py-1 font-medium ${classes}`}
+                        >
+                          {backup.type}
+                        </span>
+                      </td>
 
-                        <td className='px-6 py-4'>
-                          <span
-                            className={`inline-flex rounded-full px-3 py-1 font-medium ${classes}`}
-                          >
-                            {backup.type}
-                          </span>
-                        </td>
+                      <td className='px-6 py-4'>{FormatBytes(backup.size)}</td>
 
-                        <td className='px-6 py-4'>
-                          {FormatBytes(backup.size)}
-                        </td>
-
-                        <td className='px-6 py-4 text-center align-middle'>
-                          <ActionMenu name={backup.name} />
-                        </td>
-                      </tr>
-                    )
-                  })
-                )}
-              </tbody>
-            </table>
-          </div>
+                      <td className='px-6 py-4 text-center align-middle'>
+                        <ActionMenu name={backup.name} />
+                      </td>
+                    </tr>
+                  )
+                })
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
 
